4b2c97b587a53de48f3f4bef941d88f96f5f3371
Alex Black <wa7@sa34.cx>
2021-12-09 12:31:29 +0300

    Parallel

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+// use std::sync::Mutex;
+// use std::thread;
+use tokio::task;
+
+#[tokio::main]
+async fn main() {
+    // let counter = Mutex::new(0);
+    let mut handles = Vec::new();
+
+    for i in 1..=10 {
+        let handle = task::spawn(async move {
+            print(i).await;
+        });
+        handles.push(handle);
+    }
+
+    // for handle in handles {
+    //     handle.join().unwrap();
+    // }
+
+    // tokio::join!(handles);
+
+    for handle in handles {
+        handle.await.unwrap();
+    }
+
+    // println!("Result: {}", *counter.lock().unwrap());
+}
+
+async fn print(i: i32) {
+    println!("{}", i);
 }
